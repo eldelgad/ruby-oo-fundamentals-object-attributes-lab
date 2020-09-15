a843b4299042fcdf557d4aab43cc0f3e889dc7b6
eldelgad <evdelgado99@gmail.com>
2020-09-15 11:24:53 -0400

    dog-person-instance

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+    # def initialize(name = "Duke", breed = "Beagle")
+    #   @name = name
+    #   @breed = breed
+    # end
+  
+    def name=(name)
+      @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+  end
